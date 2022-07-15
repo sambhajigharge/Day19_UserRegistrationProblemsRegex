53defe1c1f321061c581b93de338982d15e40a33
sushant <sgharge299@gmail.com>
2022-07-15 12:04:37 +0530

    [sushant]implimented the validation of first name

--- UserRegistrationMain.java
+++ UserRegistrationMain.java
@@ -1,7 +1,21 @@
 package Regex;
 
+import java.util.Scanner;
+
 public class UserRegistrationMain {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void welcome() {
+        System.out.println("Welcome to user registration");
+    }
+
     public static void main(String[] args) {
-        System.out.println("Welcome to the User Registration");
+        welcome();
+        System.out.println("Enter 1 : to validate First Name ");
+        switch (scanner.nextInt()) {
+            case 1:
+                UserRegistration.validFirstName();
+                break;
+        }
     }
 }
